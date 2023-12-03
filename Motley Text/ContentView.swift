d0f37aa4aaf477ac1bda2f8f98c97de72a099d6c
Aleksey Sabantsev <asabantsev@gmail.com>
2023-12-03 11:47:48 +0300

    commit

--- Motley Text/ContentView.swift
+++ Motley Text/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var frameWidth: Double = 200.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(alignment: .leading) {
+                Text("Марафон ").foregroundColor(.gray) + Text("по SwiftUI")
+                Text("«Отцовский пинок»")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+            }
+            .frame(width: frameWidth)
+            .frame(minHeight: 200)
+            .padding()
+            .border(/*@START_MENU_TOKEN@*/Color.red/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+            
+            Slider(value: $frameWidth, in: 100...300)
+                .padding(.horizontal)
         }
-        .padding()
     }
 }
 
