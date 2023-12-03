7fdf83ebe77b4afdba94e3c4d225f974e55f2e2a
Aleksey Sabantsev <asabantsev@gmail.com>
2023-12-03 11:21:11 +0300

    Initial Commit

---
+++ Motley Text/Motley_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motley_TextApp.swift
+//  Motley Text
+//
+//  Created by Aleksey Sabantsev on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Motley_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
